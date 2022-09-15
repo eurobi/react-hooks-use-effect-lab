d6d115cd4d645c87723eddd7a27d7f5f94c7ae47
Jeremy Barbara <jeremy.w.barbara@gmail.com>
2022-09-15 10:18:02 -0400

    commit tree bc4338b13c45ee6b738ae923b8cd5e359ed3b071

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
@@ -11,6 +11,20 @@ function Question({ question, onAnswered }) {
   }
 
   const { id, prompt, answers, correctIndex } = question;
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => setTimeRemaining(timeRemaining-1), 1000);
+    if(timeRemaining === 0){
+      setTimeRemaining(10)
+      onAnswered(false)
+    }
+    
+    return function cleanup(){
+      clearTimeout(timeOut)
+    }
+  })
+
+
 
   return (
     <>
